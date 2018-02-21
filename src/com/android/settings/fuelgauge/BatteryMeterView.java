0d04cf07f4711db00436e1fde30e627762e2e15d
Wight554 <wight554@gmail.com>
2018-02-21 13:53:46 +0000

    Settings: Drop big circles from settings battery meter

--- src/com/android/settings/fuelgauge/BatteryMeterView.java
+++ src/com/android/settings/fuelgauge/BatteryMeterView.java
@@ -105,13 +105,11 @@ public class BatteryMeterView extends ImageView {
                     setShowPercent(false);
                     break;
                 case BatteryMeterDrawableBase.BATTERY_STYLE_CIRCLE:
-                case BatteryMeterDrawableBase.BATTERY_STYLE_BIG_CIRCLE:
                     mIntrinsicWidth = mContext.getResources().getDimensionPixelSize(R.dimen.battery_meter_height);
                     mIntrinsicHeight = mContext.getResources().getDimensionPixelSize(R.dimen.battery_meter_height);
                     showPercentInsideCircle(false);
                     break;
                 case BatteryMeterDrawableBase.BATTERY_STYLE_DOTTED_CIRCLE:
-                case BatteryMeterDrawableBase.BATTERY_STYLE_BIG_DOTTED_CIRCLE:
                     mIntrinsicWidth = mContext.getResources().getDimensionPixelSize(R.dimen.battery_meter_height);
                     mIntrinsicHeight = mContext.getResources().getDimensionPixelSize(R.dimen.battery_meter_height);
                     showPercentInsideCircle(false);
